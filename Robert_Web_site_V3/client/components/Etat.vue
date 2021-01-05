ce6301e96b02f0418d65ee96e32dacf7cecfabd0
ktazi <kenza.tazi@efrei.net>
2021-01-05 20:30:50 +0100

    squelette

--- Robert_Web_site_V3/client/components/Etat.vue
+++ Robert_Web_site_V3/client/components/Etat.vue
@@ -1,14 +1,27 @@
 <template>
-
-<div>
+  <div>
     	<nav>
       <ul>
         <li><router-link to="/etat">Etat</router-link></li>
          <li><router-link to="/conseil">Conseil</router-link></li>      
       </ul>
     </nav>
-     <h1>ETAT TEST</h1>
-</div>
+     <div id="badg">
+        <h1>Hauts faits</h1>
+        <!-- liste d'image qui sont les badges -->
+       <ul v-for="badge in badges">
+
+       </ul>
+     </div>
+    <div id="etat">
+      <div id = "date">
+
+      </div>
+      <div id="eau">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 
@@ -17,7 +30,8 @@ module.exports = {
   
   data () {
     return {
-      
+      badges : [],
+
     }
   }
 }
--- Robert_Web_site_V3/server/routes/api.js
+++ Robert_Web_site_V3/server/routes/api.js
@@ -6,7 +6,7 @@ const { Client } = require('pg')
 const client = new Client({
     user: 'postgres',
     host: 'localhost',
-    password: 'djihane89', // !!!!!! Modifiez le password !!!!!!
+    password: 'test',
     database: 'Robert'
 })
 
